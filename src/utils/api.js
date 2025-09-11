4bff18ec0a59de56d3498c09d7d84363cd32443e
Sayeed Munees <sayeedmunees0@gmail.com>
2025-09-11 16:46:24 +0530

    Updated the website

--- src/utils/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchAvailableTimes(date) {
-  // Sample logic: weekdays vs weekends
-  const weekday = ['17:00','18:00','19:00','20:00','21:00'];
-  const weekend = ['16:00','17:30','19:00','20:30'];
-  const day = new Date(date).getDay();
-  return (day === 0 || day === 6) ? weekend : weekday;
-}
